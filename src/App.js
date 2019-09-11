e829632801050c7e5e651eb681273e7505f8e07b
Dien Hoai <hoaidien93@gmail.com>
2019-09-11 13:40:37 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PlusComponent from './Component/PlusComponent';
+
+function App() {
+  return (
+    <div className="App">
+		<PlusComponent/>
+    </div>
+  );
+}
+
+export default App;
